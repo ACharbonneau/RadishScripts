55741a0142ca4eccf5ae8612391dd75436a85900
Amanda Charbonneau <charbo24@msu.edu>
2013-03-07 16:34:21 -0500

    Updated to output .pdfs and fixed directories

--- MarkerAnalysis/STRUCTURE/evanno.R
+++ MarkerAnalysis/STRUCTURE/evanno.R
@@ -1,11 +1,25 @@
+#This takes output from the StructureHarvester program and plots the DeltaK (evanno method) and mean LnP (normal method) of determining the best K. The LnP should give roughly the same answer as the final plot of PlotKprobabilities. 
 
-source('~/Documents/RadishData/RadishScripts/Misc_scripts/AmandaSource.R', chdir = TRUE)
+source('/Volumes/Storage/RadishData/RadishScripts/Misc_scripts/AmandaSource.R', chdir = TRUE)
 
-evanno <- read.csv("~/Documents/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/HarvestCorrNoRA/evanno.txt", skip=15, header=T, sep="	")
+evanno <- read.csv("/Volumes/Storage/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/HarvestCorrNoRA/evanno.txt", skip=15, header=T, sep="	")
 
 colnames(evanno) <- c("K", "Reps", "MeanLnP", "StDev", "Ln_K", "Ln__K", "DeltaK")
 
-plot(evanno$K, evanno$MeanLnP)
-plot(evanno$K, evanno$DeltaK)
+pdf(file="/Volumes/Storage/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/Plots/EvannoProbOfK.pdf")
+plot(evanno$K, evanno$DeltaK, main = "Best K using Evanno Method", ylab="DeltaK", xlab="Proposed K")
+text(x= evanno$K, y= evanno$DeltaK, pos=1, cex=.7)
+
+plot(evanno$K, evanno$MeanLnP, main = "Best K using mean Ln P", ylab = "Ln probability", xlab="Proposed K")
+text(x= evanno$K, y= evanno$MeanLnP, pos=1, cex=.7)
+
+dev.off()
+
+
+
+
+
+
 plot(evanno$K, evanno$Ln_K)
 plot(evanno$K, evanno$Ln__K)
+plot(evanno$K, evanno$MeanLnP)
