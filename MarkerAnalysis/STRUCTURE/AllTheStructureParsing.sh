25a551ee2aa127059a5fd33e1f937d37a56edced
Amanda Charbonneau <charbo24@msu.edu>
2014-01-22 15:24:41 -0500

    Made a new parsing file that actually does all the parsing instead of just most of it

--- MarkerAnalysis/STRUCTURE/AllTheStructureParsing.sh
+++ MarkerAnalysis/STRUCTURE/AllTheStructureParsing.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 for i in `ls *_f`
+	do echo $i
+	grep -A 2 "Estimated Ln Prob of Data" $i
+	done > AlltheProbabilities.txt
+
+for i in `ls *_f`
 	do grep -A 269 "Inferred ancestry of individuals" $i > $i.forparse
 done
 
@@ -9,6 +14,12 @@ for i in `ls *forparse`
 done
 
 mkdir parsed_data
+mkdir forparse
+mkdir raw_output
+mkdir error_output
+
 
 mv *_f.parsed parsed_data/
-
+mv *_f.forparse forparse
+mv *_f raw_output
+mv *.[eo]* error_output
