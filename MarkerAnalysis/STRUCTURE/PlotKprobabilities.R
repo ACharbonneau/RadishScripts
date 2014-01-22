53c7b20fae6f22b63df46c66478125e041ad1080
Amanda Charbonneau <charbo24@msu.edu>
2014-01-22 15:26:21 -0500

    Made the file more general, can do any number of K

--- MarkerAnalysis/STRUCTURE/PlotKprobabilities.R
+++ MarkerAnalysis/STRUCTURE/PlotKprobabilities.R
@@ -11,34 +11,38 @@ source('/Volumes/Storage/RadishData/RadishScripts/Misc_scripts/AmandaSource.R', 
 
 #Prob_data <- read.csv("/Volumes/Storage/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/Corrfreq/NoRACoNoPopJackknife/Probabilities/combo.csv", header=F, sep=",")
 
-Prob_data <- read.csv("/Volumes/Storage/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/Corrfreq/NoRANoNZIL/AllrunProbabilities.txt", header=F,sep=",")
+Prob_data <- read.csv("/Volumes/Storage/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/Corrfreq/NoRANoNZIL/AlltheProbabilities.txt", header=F,sep=",")
 
 colnames(Prob_data) <- c("RunNumber", "KNumber", "Estimated_Ln_Prob_of_Data", "Mean_value_of_ln_likelihood", "Variance_of_ln_likelihood")
 
 
+
 probs <- list()
-#for(i in seq(1,20)){
-for(i in levels(Prob_data$RunNumber)){
-probs[[i]] <- cbind(Prob_data$KNumber[Prob_data$RunNumber==i], Prob_data$Estimated_Ln_Prob_of_Data[Prob_data$RunNumber==i],ApproxBayesFac(Prob_data$Estimated_Ln_Prob_of_Data[Prob_data$RunNumber==i])
+for(i in levels(as.factor(Prob_data$RunNumber))){
+	probs[[i]] <- cbind(Prob_data$KNumber[Prob_data$RunNumber==i], 	Prob_data$Estimated_Ln_Prob_of_Data[Prob_data$RunNumber==i],
+	ApproxBayesFac(Prob_data$Estimated_Ln_Prob_of_Data[Prob_data$RunNumber==i])
 )
 }
 
-pdf(file="/Volumes/Storage/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/Plots/ProbOfnoNZIL.pdf")
+pdf(file="/Volumes/Storage/RadishData/2005MarkerData/STRUCTURE/RedoneStructure/EstimateK/Corrfreq/NoRANoNZIL/NZILconfAlpha.pdf")
 
-#for(i in seq(1,20)){
-for(i in levels(Prob_data$RunNumber)){
-plot(probs[[i]][ order(probs[[i]][,1]) ,3]#probs[[i]][,3]
-, xlab="Proposed K", ylab="Approximate Bayes Factor", main=paste("Run", i, sep=" "))
-text(x=seq(1,20), y=probs[[i]][ order(probs[[i]][,1]) ,3]#probs[[i]][,3]
-, pos=1, cex=.7)
+for(i in levels(as.factor(Prob_data$RunNumber))){
+	plot(probs[[i]][ order(probs[[i]][,1]) ,3]#probs[[i]][,3]
+	,xlab="Proposed K", ylab="Approximate Bayes Factor", 
+	main=paste("Run", i, sep=" "))
+
+text(x=seq(1,max(Prob_data$KNumber)), 
+	y=probs[[i]][ order(probs[[i]][,1]),3]#probs[[i]][,3]
+	, pos=1, cex=.7)
 }
 
-BestK = rep(NA, length(levels(Prob_data$RunNumber)))
-for(i in seq(1,length(levels(Prob_data$RunNumber)))){
-	BestK[i] <- probs[[i]][,1] [probs[[i]][,3]== max(probs[[i]][,3])]
+BestK = rep(NA, length(levels(as.factor(Prob_data$RunNumber))))
+
+for(a in seq(1,length(levels(as.factor(Prob_data$RunNumber))))){
+	BestK[a] <- probs[[a]][,1] [probs[[a]][,3]== max(probs[[a]][,3])]
 }
 
-BestK = BestK[seq(1,length(levels(Prob_data$RunNumber)))]
+BestK = BestK[seq(1,length(levels(as.factor(Prob_data$RunNumber))))]
 plot(density(BestK, bw=.4), main= "Density Plot of Best Ks")
 
 
