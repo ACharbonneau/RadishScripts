9598c3e60de0b6fc943f5064f54c5339db187843
Amanda Charbonneau <charbo24@msu.edu>
2014-03-10 12:36:28 -0400

    Now with pretty colors and matched graphs

--- FieldDataAnalysis/2013summerTake1.R
+++ FieldDataAnalysis/2013summerTake1.R
@@ -1,45 +1,62 @@
 #Prelimanary poking at summer 2013 dataset
+rm( list=ls())
 
 require(RColorBrewer)
 source('/Volumes/Storage/RadishData/RadishScripts/Misc_scripts/Functionarium.R', chdir = TRUE)
 
-plants2013 <- read.csv("/Users/Amanda/Dropbox/RadishWeedEvolution/Summer2013/2013plantData.csv", sep=",", na.strings="")
+spring2013 <- read.csv("/Volumes/Storage/RadishData/Summer2013Planting/2013plantsSpring.csv", sep=",", na.strings="")
+
+fall2013 <- read.csv("/Volumes/Storage/RadishData/Summer2013Planting/2013plantsFall.csv", sep=",", na.strings="")
+
 
 # Make dates into something R understands
-plants2013$GD <- strptime(plants2013$Germ.Date, format="%d-%b")
-plants2013$FD <- strptime(plants2013$Flower..Date, format="%d-%b")
-plants2013$DTF <- plants2013$FD - plants2013$GD
+
+
+spring2013$GD <- as.POSIXct( strptime(spring2013$Germ_Date, format="%m/%d/%y"))
+spring2013$BD <- as.POSIXct(strptime(spring2013$Bolt_Date, format="%m/%d/%y"))
+spring2013$FD <- as.POSIXct(strptime(spring2013$Flower_Date, format="%m/%d/%y"))
+spring2013$blossom <- as.POSIXct(strptime(spring2013$Blossom_Collected, format="%m/%d/%y"))
+spring2013$DTF <- spring2013$FD - spring2013$GD
+
+
+fall2013$GD <- as.POSIXct( strptime(fall2013$Germ_Date, format="%m/%d/%y"))
+fall2013$BD <- as.POSIXct( strptime(fall2013$Bolt_Date, format="%m/%d/%y"))
+fall2013$FD <- as.POSIXct( strptime(fall2013$Flower_Date, format="%m/%d/%y"))
+fall2013$blossom <- as.POSIXct( strptime(fall2013$Blossom_Collected, format="%m/%d/%y"))
+fall2013$DTF <- fall2013$FD - fall2013$GD
+
+
 
 
 # Look at some summary stats
-summary(plants2013)
+summary(spring2013)
 
 # Germination rates
-germtable <- table(plants2013$Name, plants2013$Germ.Date)
+germtable <- table(spring2013$Name, spring2013$Germ_Date)
 summary(germtable)
 germinated <- apply(germtable, 1, sum)
 germtable
 
 # Survival
-diedtable <- table(plants2013$Name, plants2013$Died)
+diedtable <- table(spring2013$Name, spring2013$Died)
 died <- apply(diedtable, 1, sum)
 died
 germinated - died
 
 # DTF
-dtftable <- table(plants2013$Name, plants2013$DTF)
-plot(plants2013$DTF[plants2013$Name=="RACA"], seq(1,10))
-plants2013$DTF[plants2013$Name=="RACA"]
+dtftable <- table(spring2013$Name, spring2013$DTF)
+plot(spring2013$DTF[spring2013$Name=="RACA"], seq(1,10))
+spring2013$DTF[spring2013$Name=="RACA"]
 
 ## Plot DTF
-par(mfrow= c(6,5))
-populations <- levels(plants2013$Name)
+par(mfrow= c(6,5), mar=c(2,2,2,2))
+populations <- levels(spring2013$Name)
 
 for(pop in populations){
 
 MTpop <- rep(NA, 10)	 #make an empty set for each population so they'll all have 10 data points even if they haven't 	
 
-current <- sort(plants2013$DTF[plants2013$Name==pop]) #See if there are any that have flowered
+current <- sort(spring2013$DTF[spring2013$Name==pop]) #See if there are any that have flowered
 
 if(length(current) > 0){
 	MTpop[seq(1,length(current))] <- current
@@ -50,7 +67,9 @@ if(length(current) > 0){
 	ylim=c(0,10), xlim=c(15, 60),
 	xlab=c(pop, "DTF"), ylab=c(length(current), "flowered"),
 	pch=16, col="blue")	
-} #Plot all the ones that flowered, plus the NA's so all the graphs have the same dim.
+} 
+
+#Plot all the ones that flowered, plus the NA's so all the graphs have the same dim.
 
 #  Plot DTF by Height of first flower
 
@@ -58,12 +77,12 @@ par(mfrow= c(6,5))
 
 for(pop in populations){
 	
-current <- sort(plants2013$DTF[plants2013$Name==pop])
+current <- sort(spring2013$DTF[spring2013$Name==pop])
 
-	plot(x=plants2013$DTF[plants2013$Name==pop],
-	y=plants2013$Height.1st..Flower..cm.[plants2013$Name==pop],
-	ylim= range(as.numeric(plants2013$Height.1st..Flower..cm.), na.rm=T),
-	xlim=range(as.numeric(plants2013$DTF), na.rm=T),
+	plot(x=spring2013$DTF[spring2013$Name==pop],
+	y=spring2013$Height_1st_Flower_cm[spring2013$Name==pop],
+	ylim= range(as.numeric(spring2013$Height_1st_Flower_cm), na.rm=T),
+	xlim=range(as.numeric(spring2013$DTF), na.rm=T),
 	xlab=c(pop, "DTF"), ylab=c("flower height (cm)"),
 	pch=16, col="blue")	
 }
@@ -71,9 +90,6 @@ current <- sort(plants2013$DTF[plants2013$Name==pop])
 
 ######  Plot proportion flowered w/o vernalization rather than by DTF #####
 
-par(mfrow= c(1,1), las=3)
-palette <- brewer.pal(c(6),"Set1")
-palette <- palette[c(1,3,5)] #using the same colors as the 2012 data set
 
 w <- "Weed"
 n <- "Native"
@@ -98,19 +114,34 @@ proportions$SE <- as.numeric(proportions$SE)
 	#Fill the table with proportion flowered, N and SE
 for(pop in populations){
 	proportions[proportions[,1]==pop,2] <-CalcProp(
-		plants2013$Flower..Date[plants2013$Name==pop],
-		plants2013$Germ.Date[plants2013$Name==pop])
-	proportions[proportions[,1]==pop,3] <- length(na.omit(plants2013$Germ.Date[plants2013$Name==pop]))
+		spring2013$FD[spring2013$Name==pop],
+		spring2013$GD[spring2013$Name==pop])
+		
+	proportions[proportions[,1]==pop,3] <- length(na.omit(spring2013$GD[spring2013$Name==pop]))
 	proportions[proportions[,1]==pop,5] <- SEP(
 		proportions[proportions[,1]==pop,2], proportions[proportions[,1]==pop,3])
 }
-	# Order the pops by their proportion flowered to make it pretty. Group pops.
+
+#######################################################
+
+######## Distribution of %flower by Ecotype #########
+par(mfrow= c(1,1), las=3)
+palette <- brewer.pal(c(6),"Set1")
+palette <- palette[c(1,3,5)] #using the same colors as the 2012 data set
 
 bytype <- order(proportions$prop,proportions$prop[proportions$type], decreasing=T)
 
-	#Plot as bargraph.
-barplot(as.numeric(proportions$prop)[bytype], names.arg= proportions$populations[bytype], col=palette[proportions$type][bytype], ylim=c(-.03,1), xlim=c(-.15, 32), ylab="Proportion Flowered", main="Spring 2013") 
+barplot(as.numeric(proportions$prop)[bytype], 
+	names.arg= proportions$populations[bytype], 
+	col=palette[proportions$type][bytype], 
+	ylim=c(-.03,1), xlim=c(-.15, 32), 
+	ylab="Proportion Flowered", main="Spring 2013") 
+	
 legend(25, .8,legend=levels(as.factor(proportions$type)),pch=16, col=palette, bty="n")
+
+
+#######################################################
+
 
 
 xcords <- seq(.7, 32, by=1.2)
